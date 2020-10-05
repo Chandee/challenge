6c22016834f5c905bcf6193352712478acf0ba82
alexandre <chandemoraes@hotmail.com>
2020-10-04 22:48:36 -0300

    Updates

---
+++ precache-manifest.ff0786c3cf6bd04b085ec084809c0295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1074c20c98010aa016430db24d893c",
+    "url": "/challenge/index.html"
+  },
+  {
+    "revision": "00bd1de7f014fe14531a",
+    "url": "/challenge/static/css/main.7e37e131.chunk.css"
+  },
+  {
+    "revision": "7dd86e4da878fee5b381",
+    "url": "/challenge/static/js/2.1bd64e9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/challenge/static/js/2.1bd64e9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00bd1de7f014fe14531a",
+    "url": "/challenge/static/js/main.60e030f9.chunk.js"
+  },
+  {
+    "revision": "18b3540ad4403f284dad",
+    "url": "/challenge/static/js/runtime-main.7a3372df.js"
+  }
+]);
